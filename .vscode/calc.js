aa6d33358f804ee09c489b82ef2f479a6be94ae5
Deepali gupta <95581953+Deepaligup-ta@users.noreply.github.com>
2023-02-22 00:14:42 +0530

    Update calc.js

--- .vscode/calc.js
+++ .vscode/calc.js
@@ -1,5 +1,5 @@
 console.log(
-  "Javascript Calculator Made by Harsh Trivedi\nhttps://harsh98trivedi.github.io"
+  "Javascript Calculator Made by Deepali Gupta"
 );
 let flag = 0;
 function isNumber(char) {
